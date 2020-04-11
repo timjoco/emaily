ca739ad0e4a4af77c5931d1af2cc166b18259d76
Timjoco <timjoco13@gmail.com>
2020-04-11 18:34:53 -0500

    added google oauth functionality

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send({ bye: 'friend' });
-});
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        }, 
+        accessToken => {
+            console.log(accessToken)
+        }
+    )
+);
 
-// Boolean statement for nodeJS environment port OR port 5000
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email'] // Specifies to google what access we want - in this case, profile and email - from a users account
+    })
+);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
