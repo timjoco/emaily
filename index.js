ea13ddb81cb0648023df4ec9a82468d22fbc1ab7
Timjoco <timjoco13@gmail.com>
2020-04-11 14:35:42 -0500

    changed greeting

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send({ hi: 'there' });
+    res.send({ bye: 'friend' });
 });
 
+// Boolean statement for nodeJS environment port OR port 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
