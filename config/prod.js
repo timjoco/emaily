94516ede78a37382782ad28c8260f15aa6d566e6
Timjoco <timjoco13@gmail.com>
2020-04-12 20:39:34 -0500

    completed authentication and added production keys seperate from development keys for sensitive data

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
+
+// mongodb+srv://timprod:FzpaomrDdrJ8KDxu@cluster0-llpkl.mongodb.net/test?retryWrites=true&w=majority
+
