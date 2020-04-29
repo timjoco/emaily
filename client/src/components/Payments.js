a4e136a5910b63b74ca49d4c7223349ec98295ea
Timjoco <timjoco13@gmail.com>
2020-04-29 13:02:49 -0500

    Corrected .env files to show correct Stripe API key

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react'
-import StripeCheckout from 'react-stripe-checkout'
-import { connect } from 'react-redux'
-import * as actions from '../actions'
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Payments extends Component {
   render() {
     return (
-      <StripeCheckout 
+      <StripeCheckout
         name="Emaily"
         description="$5 for 5 Email Credits"
         amount={500}
-        token={token => this.props.handleToken(token)}
+        token={(token) => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
-        <button className="btn">
-          Add Credits
-        </button>
+        <button className="btn">Add Credits</button>
       </StripeCheckout>
-    )
+    );
   }
 }
 
